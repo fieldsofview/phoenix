3b309b127689808e728866381cdaf6672a70d5fc
Harsha <source@kharsha.com>
2013-03-14 17:28:42 +0530

    Modified some of murali's Log code and changed Boot to use only one machine file.

--- src/System/Boot.java
+++ src/System/Boot.java
@@ -1,3 +1,7 @@
+/**
+ * The Boot Class sets up the start of a simulation process. This class contains
+ * methods to perform checks for modules.
+ */
 package System;
 
 import java.io.BufferedReader;
@@ -5,52 +9,61 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Map;
 import Communication.ACNetwork;
 import Communication.QueueParameters;
 
 /**
  * This class loads the configurations for a given agent controller.
  */
-//TODO : Set up Logger everywhere
+// TODO : Set up Logger everywhere
 public class Boot {
 
-    /**
-     * Loads the configurations for the machine. Each machine runs a 
-     * AgentController. AgentControllerhandles communication and 
-     * agent behaviour.
-     *
-     * @param name File name containing the care taker agent configuration.
-     * @throws IOException
-     * @throws FileNotFoundException
-     * @throws NumberFormatException
-     */
-    public static void loadMachineConfigurations(String name) throws IOException, FileNotFoundException, NumberFormatException {
+	/**
+	 * Loads the configurations for the machine. Each machine runs a
+	 * AgentController. AgentControllerhandles communication and agent
+	 * behaviour.
+	 * 
+	 * @param name
+	 *            File name containing the care taker agent configuration.
+	 * @throws IOException
+	 * @throws FileNotFoundException
+	 * @throws NumberFormatException
+	 */
+	public static void loadMachineConfigurations(String name)
+			throws IOException, FileNotFoundException, NumberFormatException {
 
-        BufferedReader inputFile;
-        inputFile = new BufferedReader(new FileReader(name));
-        String inputLine;
+		BufferedReader inputFile;
+		inputFile = new BufferedReader(new FileReader(name));
+		String inputLine;
 
-        while ((inputLine = inputFile.readLine()) != null) {
-            String[] param = inputLine.split(":");
-            int type = Integer.parseInt(param[0]);
-            ACNetwork.agentControllerhostList.add(param[1]);
-            QueueParameters queueParameters = new QueueParameters(param[2], param[3], param[4], param[5], param[6], param[7], param[8]);
+		while ((inputLine = inputFile.readLine()) != null) {
+			String[] param = inputLine.split(":");
+//			int type = Integer.parseInt(param[0]);
+			ACNetwork.agentControllerhostList.add(param[1]);
+			QueueParameters queueParameters = new QueueParameters(param[2],
+					param[3], param[4], param[5], param[6], param[7], param[8]);
 
-            ACNetwork.hostMessageQueueLookup.put(param[1], queueParameters);
+			ACNetwork.hostMessageQueueLookup.put(param[1], queueParameters);
 
-            //Build a directory of hosts based on their type for communication.
-            Map<Integer, ArrayList<String>> hostMap = Communication.ACNetwork.hostTypeMap;
-            ArrayList<String> hosts = new ArrayList<String>(); //The list of hosts of a certain type.
-            if (hostMap.containsKey(type)) {
-                hosts = hostMap.get(type); //If the type is already present append arraylist.
-            }
-            //If not add a new map entry.
-            hosts.add(param[1]);
-            hostMap.put(type, hosts);
-        }
-
-        //Close the file
-        inputFile.close();
-    }
+			// Build a directory of hosts based on their type for communication.
+			//Map<Integer, ArrayList<String>> hostMap = Communication.ACNetwork.hostTypeMap;
+			//Build a directory of hosts
+			ArrayList<String> hostMap;
+			//ArrayList<String> hosts = new ArrayList<String>(); // The list of
+																// hosts of a
+																// certain type.
+			hostMap = new ArrayList<String>();
+			
+			//if (hostMap.containsKey(type)) {
+			//	hosts = hostMap.get(type); // If the type is already present
+											// append arraylist.
+			//}
+			// If not add a new map entry.
+			//hosts.add(param[1]);
+			//hostMap.put(type, hosts);
+			hostMap.add(param[1]);
+		}
+		// Close the file
+		inputFile.close();
+	}
 }
--- src/System/Log.java
+++ src/System/Log.java
@@ -1,72 +1,30 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * This is the Logging System Class that generates a logger instance for anything
+ * that requires to be logged in the system. The Logger used if Apache's log4j 1.2.17
  */
-package Utilities;
+package System;
 
-import Agents.Agent;
-import Communication.QueueManager;
-import Database.DBAccess;
-import System.AIDGenerator;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.TimeZone;
-import org.apache.log4j.FileAppender;
-import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.apache.log4j.PatternLayout;
 import org.apache.log4j.PropertyConfigurator;
 
-
-
 /**
- *
+ * 
  * @author Murali
  */
 public class Log {
 
+	public static Logger logger = Logger.getRootLogger();
 
-    public static Logger logger;
+	/*
+	 * TODO:// Write a on the fly log files generator.
+	 */
 
-
-/**
- * 
- * @param object of a class
- * @return logger 
- * @description method to return logger based on the class object type
- */
-    public static Logger returnLogger(Object object){
-      
-        if(object instanceof Agents.Agent){
-            return Logger.getLogger(Agent.class);
-        }else if(object instanceof  DBAccess) {
-            return Logger.getLogger(DBAccess.class);
-        }else if(object instanceof   AIDGenerator)
-            return Logger.getLogger(AIDGenerator.class);
-        else if(object instanceof QueueManager )
-            return  Logger.getLogger(QueueManager.class);
-        return null;
-        
-    }
-    
-        /**
-     * Configure the logger. 
-     */
-    public static void ConfigureLogger() {
-        
-        PropertyConfigurator.configure("src/Utilities/logger.properties");     
-        //TimeZone tz = TimeZone.getTimeZone("EST"); // or PST, MID, etc ...
-        Date now = new Date();
-        //DateFormat df = new SimpleDateFormat("yyyy.MM.dd hh:mm ");
-        DateFormat df = new SimpleDateFormat("yyyy.MM.dd");
-        
-        String currentTime = df.format(now);
-
-        FileAppender appender = null;
-        PatternLayout p = new PatternLayout("%d %-5p [%t] %-17c{2} (%13F:%L) %3x - %m%n");
-      //  logger.setLevel(Level.INFO);
-        //logger.info("logging initialized");
-    }
-
+	/**
+	 * Configure the logger based on the configuration file provided in the
+	 * "config" directory.
+	 */
+	public static void ConfigureLogger() {
+		PropertyConfigurator.configure("src/System/logger.properties");
+		// TimeZone tz = TimeZone.getTimeZone("IST");
+	}
 }
