2f17abd46d87e73d1e15b6af48092621defe26a7
Harsha <source@kharsha.com>
2013-03-14 16:47:53 +0530

    Added Murali's changes to the Log system.

--- src/System/Log.java
+++ src/System/Log.java
@@ -1,30 +1,72 @@
 /*
- * Old logger file that will be removed to include the one developed by murali.
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
  */
-package System;
+package Utilities;
 
+import Agents.Agent;
+import Communication.QueueManager;
+import Database.DBAccess;
+import System.AIDGenerator;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+import org.apache.log4j.FileAppender;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
 import org.apache.log4j.PropertyConfigurator;
 
 
+
 /**
- * System wide Logger
+ *
+ * @author Murali
  */
 public class Log {
-    /**
-     * Logger, level set at info
-     */
-    public static Logger logger = Logger.getRootLogger();
 
-    /**
-     * Configure the logger. To be called before anything else
+
+    public static Logger logger;
+
+
+/**
+ * 
+ * @param object of a class
+ * @return logger 
+ * @description method to return logger based on the class object type
+ */
+    public static Logger returnLogger(Object object){
+      
+        if(object instanceof Agents.Agent){
+            return Logger.getLogger(Agent.class);
+        }else if(object instanceof  DBAccess) {
+            return Logger.getLogger(DBAccess.class);
+        }else if(object instanceof   AIDGenerator)
+            return Logger.getLogger(AIDGenerator.class);
+        else if(object instanceof QueueManager )
+            return  Logger.getLogger(QueueManager.class);
+        return null;
+        
+    }
+    
+        /**
+     * Configure the logger. 
      */
     public static void ConfigureLogger() {
         
         PropertyConfigurator.configure("src/Utilities/logger.properties");     
-        logger.setLevel(Level.INFO);
-        logger.info("logging initialized");
+        //TimeZone tz = TimeZone.getTimeZone("EST"); // or PST, MID, etc ...
+        Date now = new Date();
+        //DateFormat df = new SimpleDateFormat("yyyy.MM.dd hh:mm ");
+        DateFormat df = new SimpleDateFormat("yyyy.MM.dd");
+        
+        String currentTime = df.format(now);
+
+        FileAppender appender = null;
+        PatternLayout p = new PatternLayout("%d %-5p [%t] %-17c{2} (%13F:%L) %3x - %m%n");
+      //  logger.setLevel(Level.INFO);
+        //logger.info("logging initialized");
     }
 
 }
