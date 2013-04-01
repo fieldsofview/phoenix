bf574975c723fd6bae303b62b4bf3876fc1b1f21
Onkar Hoysala <mail@onkarhoysala.com>
2013-04-01 17:09:36 +0530

    Module interface added

---
+++ src/Module/module.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package Module;
+
+/**
+ * @author onkar
+ * @version 0.1
+ *Date of creation: 1 April 2013
+ *This is the module interface. All Phoenix modules must implement this
+ *interface. Custom methods for each module must be provided by the
+ *module.
+ */
+public interface module {
+	/**
+	 * This method will boot the module. It should check for other module
+	 * dependencies.
+	 */
+	void boot();
+
+	/**
+	 * This method will run the initialisation script for the module.
+	 */
+	void initialise();
+}
