0c97cbf71ae69f189644f56a2c869da9ebcf44be
Onkar Hoysala <mail@onkarhoysala.com>
2013-04-04 12:38:08 +0530

    Deleted DBAccess.java. Tested DatabaseModule

--- src/module/database/pooler/DatabasePoolFactory.java
+++ src/module/database/pooler/DatabasePoolFactory.java
@@ -1,19 +1,13 @@
 package module.database.pooler;
 
+
 import java.util.Properties;
 
 public class DatabasePoolFactory {
 	public static DatabasePooler getPooler(String poolerClass, Properties p){
-		try {
-			@SuppressWarnings("rawtypes")
-			Class c= Class.forName(poolerClass);
-			return ((DatabasePooler) c.newInstance()).initialise(p);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (InstantiationException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
+		System.out.println("Loading pooler: "+poolerClass);
+		if (poolerClass.equals("module.database.pooler.BoneCPPooler")){
+			return BoneCPPooler.getInstance(p);
 		}
 		return null;
 	}
