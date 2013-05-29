fb568c8d8ed7cbc944f2e3a1f73cb47319b40e72
Onkar Hoysala <mail@onkarhoysala.com>
2013-05-29 15:31:58 +0530

    Added tested code for database module

--- src/module/database/DatabaseModule.java
+++ src/module/database/DatabaseModule.java
@@ -3,55 +3,99 @@ package module.database;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 import module.Module;
 import module.database.pooler.DatabasePoolFactory;
 import module.database.pooler.DatabasePooler;
 
-public class DatabaseModule implements Module {
 
-	DatabasePooler pooler = null;
-	Properties p = null;
+public class DatabaseModule implements Module{
 
-	public DatabaseModule() {
-		// First read properties file
-		Properties p = new Properties();
-		try {
-			p.load(new FileInputStream("database.properties"));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		// Next call boot
-		boot();
-	}
+        DatabasePooler pooler=null;
+        Properties p=null;
+        public String updateQuery;
+        public String query;
+        
+        public DatabaseModule() {
+                //First read properties file
+                System.out.println("Booting database module.\nLoading properties file.");
+                p=new Properties();
+                try {
+                        p.load(new FileInputStream("config/database.properties"));
+                } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }
+                //Next call boot
+                boot();
+        }
 
-	/**
-	 * Do boot-time checking of dependencies
-	 */
-	@Override
-	public void boot() {
-		// Dependency check
-		try {
-			ClassLoader l = ClassLoader.getSystemClassLoader();
-			Class.forName("com.jdbc.mysql.Driver", false, l);
-			Class.forName("com.jolbox.bonecp.BoneCP", false, l);
-			Class.forName("org.slf4j.Logger", false, l);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-			// TODO add error log
-			// TODO system.exit
-		}
-		// After successful boot call initialise
-		initialise();
-	}
+        /**
+         * Do boot-time checking of dependencies
+         */
+        @Override
+        public void boot() {
+                System.out.println("Running boot and depdendency check.");
+                //Dependency check
+                try {
+                        ClassLoader l=ClassLoader.getSystemClassLoader();
+                        //Class.forName("com.jdbc.mysql.Driver",false,l);
+                        //Class.forName("com.jolbox.bonecp.BoneCP",false,l);
+                        Class.forName("org.slf4j.Logger",false,l);
+                } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                        //TODO add error log
+                        //TODO system.exit
+                }
+                //After successful boot call initialise
+                initialise();
+        }
 
-	@Override
-	public void initialise() {
-		pooler = DatabasePoolFactory.getPooler(p.getProperty("pooler")
-				.toString(), p);
-	}
-
+        @Override
+        public void initialise() {
+                System.out.println("Initialising database module and pooler.");
+                try {
+                        pooler=DatabasePoolFactory.getPooler(p.getProperty("pooler").toString(),p);
+                } catch (IllegalArgumentException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                } catch (SecurityException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                }
+                
+        }
+        
+        public ResultSet execute(){
+                try {
+                        Connection c=this.pooler.getConnection();
+                        Statement s=c.prepareStatement(query);
+                        return s.executeQuery(query);
+                } catch (SQLException e) {
+                        e.printStackTrace();
+                }
+                return null;
+        }
+        
+        public Integer executeUpdate(){
+                try{
+                        Connection c=this.pooler.getConnection();
+                        return c.prepareStatement(updateQuery).executeUpdate();
+                } catch (SQLException e){
+                        e.printStackTrace();
+                }
+                return null;
+        }
+        
+        //TODO: remove this main
+        public static void main(String args[]){
+                DatabaseModule d=new DatabaseModule();
+                System.out.println(d.pooler.toString());
+        }
 }
